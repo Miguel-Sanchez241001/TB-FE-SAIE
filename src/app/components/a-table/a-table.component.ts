fdbfc6c469f4e9d9baa434f96e243317308a4940
Miguel-Sanchez241001 <msanchez24@autonoma.edu.pe>
2024-05-03 11:06:44 -0500

    agrgando componente de tabla

--- src/app/components/a-table/a-table.component.ts
+++ src/app/components/a-table/a-table.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { EntidadModel, EntidadModelImpl, InterfazModel } from '../../models/model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {  HttpClientModule } from '@angular/common/http';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-a-table',
   templateUrl: './a-table.component.html',
   styleUrl: './a-table.component.css',
-  standalone:true
+  standalone:true,
+  imports:[FormsModule,CommonModule,HttpClientModule ]
+
 })
-export class ATableComponent {
+export class ATableComponent implements OnInit {
+  @Input() datos: (EntidadModel | InterfazModel)[] = [];
+  columnas: string[] = [];
+  entidades:EntidadModel[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+
+    this.apiService.getOptions().subscribe(
+      (entidades: EntidadModelImpl[]) => {
+        console.log({entidades});
+       entidades.map((entidad)=>  {
+           this.entidades.push(entidad);
+           this.columnas = Object.keys(entidad);
+          });
+          const indice = this.columnas.indexOf('interfaces');
+          if (indice !== -1) {
+            this.columnas.splice(indice, 1);
+          }
+          console.log(this.columnas);
+      },
+      error => {
+        console.error('Error al obtener las entidades:', error);
+      }
+    );
+  }
+
+
+
+
+
+
+
+
 
 }
